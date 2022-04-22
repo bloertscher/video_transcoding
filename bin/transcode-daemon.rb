f8b65f2aa4504d6ae282df9e3f03170fda41fc12
bloertscher <loertscher.b@outlook.com>
2022-04-21 20:09:37 -0600

    Make transcode-daemon a module to avoid globals

--- bin/transcode-daemon.rb
+++ bin/transcode-daemon.rb
@@ -7,124 +7,131 @@ require 'fileutils'
 require 'json'
 require 'pathname'
 require 'logger'
+require 'video_transcoding/transcode'
 
-$logger = Logger.new($stdout)
-$queue = Queue.new
+module VideoTranscoding
+  module TranscodeDaemon
+    class TranscodeJobError < RuntimeError
+    end
 
-BASE_DIR = Pathname.new '/tmp/spool/ripper'
-WORK_DIRS = {
-  queue: BASE_DIR / 'queue',
-  running: BASE_DIR / 'running',
-  failed: BASE_DIR / 'failed',
-  succeeded: BASE_DIR / 'succeeded'
-}
+    BASE_DIR = Pathname.new '/tmp/spool/ripper'
+    WORK_DIRS = {
+      queue: BASE_DIR / 'queue',
+      running: BASE_DIR / 'running',
+      failed: BASE_DIR / 'failed',
+      succeeded: BASE_DIR / 'succeeded'
+    }.freeze
 
-# Make sure the dirs exist
-WORK_DIRS.each_value do |dir|
-  FileUtils.mkdir_p dir
-end
+    def initialize
+      @logger = Logger.new($stdout)
+      @queue = Queue.new
+    end
 
-class TranscodeJobError < RuntimeError
-end
+    def read_job_options(pathname)
+      # the options for running the transcode job, specified by the JSON file
+      job_options = {}
+      # Read the File contents
+      pathname.open do |f|
+        # JSON parse to hash
+        text = f.read
+        @logger.debug "JSON file text:\n#{text}"
+        begin
+          job_options = JSON.parse(text)
+        rescue JSON::JSONError
+          @logger.error "Failed to parse JSON job file '#{pathname}'."
+          @logger.debug "Moving job file to '#{WORK_DIRS[:failed]}'"
+          FileUtils.mv pathname, WORK_DIRS[:failed]
+          raise TranscodeJobError, "Failed to parse JSON job file '#{pathname}'."
+        end
+      end
+    end
 
-def read_job_options(pathname)
-  # the options for running the transcode job, specified by the JSON file
-  job_options = {}
-  # Read the File contents
-  pathname.open do |f|
-    # JSON parse to hash
-    text = f.read
-    $logger.debug "JSON file text:\n#{text}"
-    begin
-      job_options = JSON.parse(text)
-    rescue JSON::JSONError
-      $logger.error "Failed to parse JSON job file '#{pathname}'."
-      $logger.debug "Moving job file to '#{WORK_DIRS[:failed]}'"
-      FileUtils.mv pathname, WORK_DIRS[:failed]
-      raise TranscodeJobError, "Failed to parse JSON job file '#{pathname}'."
+    def verify_options(job_options)
+      ["output", "input", "markers", "encoder", "title"].each do |key|
+        raise TranscodeJobError, "Required option '#{key}' not specified in JSON job" unless job_options.key?(key)
+      end
+    end
+
+    def move_job_file(src_file, dest_dir)
+      name = src_file.basename
+      dest_file = dest_dir / name
+      @logger.debug "Moving from '#{src_file}' to '#{dest_file}'"
+      FileUtils.mv src_file, dest_file
+      dest_file
+    end
+
+    def run_job(abs_path)
+      start_time = Time.now
+      pathname = Pathname.new(abs_path)
+      current_file = pathname
+      @logger.info "Starting job '#{pathname}'"
+
+      begin
+        job_options = read_job_options(pathname)
+        verify_options(job_options)
+        current_file = move_job_file(current_file, WORK_DIRS[:running])
+        transcode(job_options)
+      rescue RuntimeError => e
+        @logger.error "Failed transcode of #{pathname}: " + e.message
+
+        move_job_file(current_file, WORK_DIRS[:failed])
+      else
+        move_job_file(current_file, WORK_DIRS[:succeeded])
+        @logger.info "Completed job: #{job_options['output']}"
+        seconds = (Time.now - start_time).round
+        hours   = seconds / (60 * 60)
+        minutes = (seconds / 60) % 60
+        seconds = seconds % 60
+        @logger.info format("Elapsed time: %02d:%02d:%02d\n\n", hours, minutes, seconds)
+      end
+    end
+
+    def transcode(job_options)
+      @logger.info "Transcoding: #{job_options['input']}"
+
+      adjust_metadata(job_options['output'])
+    end
+
+    def adjust_metadata(output_filepath)
+      @logger.info "Adjusting metadata for #{output_filepath}"
+    end
+
+    def main()
+      # Make sure the dirs exist
+      WORK_DIRS.each_value do |dir|
+        FileUtils.mkdir_p dir
+      end
+      # Add existing files to the queue, so they don't get ignored
+      Dir.glob("*.json", base: WORK_DIRS[:queue]) { |job| @queue.push WORK_DIRS[:queue] / job }
+
+      listener = Listen.to(WORK_DIRS[:queue], only: /\.json$/) do |modified, added, _removed|
+        # For each new file, run a job on it
+        added.each do |path|
+          @queue.push path
+        end
+
+        modified.each do |path|
+          @queue.push path
+        end
+      end
+
+      job_runner = Thread.new do
+        while job = @queue.pop
+          run_job(job)
+        end
+      end
+
+      @logger.info "Starting watching for new job files in '#{WORK_DIRS[:queue]}'"
+      listener.start
+
+      sleep
+
+      @logger.info 'Transcode job processor exiting'
+      listener.stop
+      @queue.close
+      job_runner.join
     end
   end
 end
 
-def verify_options(job_options)
-  ["output", "input", "markers", "encoder", "title"].each do |key|
-    unless job_options.has_key?(key)
-      raise TranscodeJobError, "Required option '#{key}' not specified in JSON job"
-    end
-  end
-end
-
-def move_job_file(src_file, dest_dir)
-  name = src_file.basename
-  dest_file = dest_dir / name
-  $logger.debug "Moving from '#{src_file}' to '#{dest_file}'"
-  FileUtils.mv src_file, dest_file
-  dest_file
-end
-
-def run_job(abs_path)
-  start_time = Time.now
-  pathname = Pathname.new(abs_path)
-  current_file = pathname
-  $logger.info "Starting job '#{pathname}'"
-
-  begin
-    job_options = read_job_options(pathname)
-    verify_options(job_options)
-    current_file = move_job_file(current_file, WORK_DIRS[:running])
-    transcode(job_options)
-
-  rescue RuntimeError => e
-    $logger.error "Failed transcode of #{pathname}: " + e.message
-
-    move_job_file(current_file, WORK_DIRS[:failed])
-  else
-    move_job_file(current_file, WORK_DIRS[:succeeded])
-    $logger.info "Completed job: #{job_options['output']}"
-    seconds = (Time.now - start_time).round
-    hours   = seconds / (60 * 60)
-    minutes = (seconds / 60) % 60
-    seconds = seconds % 60
-    $logger.info format("Elapsed time: %02d:%02d:%02d\n\n", hours, minutes, seconds)
-  end
-end
-
-def transcode(job_options)
-  $logger.info "Transcoding: #{job_options['input']}"
-
-  adjust_metadata(job_options['output'])
-end
-
-def adjust_metadata(output_filepath)
-  $logger.info "Adjusting metadata for #{output_filepath}"
-end
-
-# Add existing files to the queue, so they don't get ignored
-Dir.glob("*.json", base: WORK_DIRS[:queue]) { |job| $queue.push WORK_DIRS[:queue] / job }
-
-listener = Listen.to(WORK_DIRS[:queue], only: /\.json$/) do |modified, added, _removed|
-  # For each new file, run a job on it
-  added.each do |path|
-    $queue.push path
-  end
-
-  modified.each do |path|
-    $queue.push path
-  end
-end
-
-job_runner = Thread.new do
-  while job = $queue.pop
-    run_job(job)
-  end
-end
-
-$logger.info "Starting watching for new job files in '#{WORK_DIRS[:queue]}'"
-listener.start
-
-sleep
-
-$logger.info 'Transcode job processor exiting'
-listener.stop
-$queue.close
-job_runner.join
+VideoTranscoding::TranscodeDaemon.main
---
+++ lib/video_transcoding/transcode.rb
@@ -0,0 +1,121 @@
+require 'video_transcoding'
+
+module VideoTranscoding
+  # @param handbrake_options [Hash] CLI options for HandBrake
+  # @param dry_run [bool] Whether to perform a dry run
+  # @param log_on [bool] Whether to log to a file or not
+  def self.transcode(handbrake_options, dry_run, log_on)
+    handbrake_command = prepare_command(handbrake_options, dry_run)
+    puts handbrake_command.join(' ') if dry_run
+
+    Console.debug handbrake_command.inspect
+    log_file_path = handbrake_options['output'] + '.log'
+    log_file = log_on ? File.new(log_file_path, 'wb') : nil
+    Console.info 'Transcoding with HandBrakeCLI...'
+
+    begin
+      IO.popen(handbrake_command, 'rb', :err => [:child, :out]) do |io|
+        Signal.trap 'INT' do
+          Process.kill 'INT', io.pid
+        end
+
+        buffer = ''
+
+        io.each_char do |char|
+          if (char.bytes[0] & 0x80) != 0
+            buffer << char
+          else
+            if not buffer.empty?
+              print buffer
+              buffer = ''
+            end
+
+            print char
+          end
+
+          log_file.print char unless log_file.nil?
+        end
+
+        if not buffer.empty?
+          print buffer
+        end
+      end
+    rescue SystemCallError => e
+      raise "transcoding failed: #{e}"
+    end
+
+    log_file.close unless log_file.nil?
+    fail "transcoding failed: #{handbrake_options['input']}" unless $CHILD_STATUS.exitstatus == 0
+
+    unless log_file.nil?
+      timestamp = File.mtime(log_file_path)
+      content = ''
+
+      begin
+        File.foreach(log_file_path) do |line|
+          content += line
+        end
+      rescue SystemCallError => e
+        raise "reading failed: #{e}"
+      end
+
+      log_file = File.new(log_file_path, 'wb')
+      log_file.print content.gsub(/^.*\r(.)/, '\1')
+      log_file.close
+      FileUtils.touch log_file_path, :mtime => timestamp
+    end
+
+    adjust_metadata(handbrake_options['output']) unless dry_run
+  end
+
+  def self.adjust_metadata(output)
+    media = Media.new(path: output, allow_directory: false)
+    Console.debug media.info.inspect
+
+    if  media.info[:mkv] and
+        media.info[:subtitle].include? 1 and
+        media.info[:subtitle][1][:default] and
+        not media.info[:subtitle][1][:forced]
+      Console.info 'Forcing subtitle with mkvpropedit...'
+
+      begin
+        IO.popen([
+                   MKVpropedit.command_name,
+                   '--edit', 'track:s1',
+                   '--set', 'flag-forced=1',
+                   output,
+                 ], 'rb', :err => [:child, :out]) do |io|
+          io.each do |line|
+            Console.debug line
+          end
+        end
+      rescue SystemCallError => e
+        raise "forcing subtitle failed: #{e}"
+      end
+
+      fail "forcing subtitle: #{output}" if $CHILD_STATUS.exitstatus == 2
+    end
+  end
+
+  # Prepares the actual HandBrakeCLI command, with all the options
+  #
+  # @param handbrake_options [Hash] CLI options for HandBrake
+  # @param dry_run [bool] If true, makes sure to shell escape args
+  def self.prepare_command(handbrake_options, dry_run)
+    handbrake_command = [HandBrake.command_name]
+
+    Console.debug handbrake_options
+    handbrake_options.each do |name, value|
+      if value.nil?
+        handbrake_command << "--#{name}"
+      elsif dry_run and name != 'encopts'
+        # Don't need to shellescape encopts I guess
+        handbrake_command << "--#{name}=#{value.shellescape}"
+      else
+        handbrake_command << "--#{name}=#{value}"
+      end
+    end
+
+    return handbrake_command
+  end
+end
